be944e0d06472c35a691c7ab96fede754e18cc22
Yuliia <taragorgodze@gmail.com>
2023-12-29 08:04:05 +0200

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,23 @@
 export default async function sendToTg(req, res) {
   if (req.method === "POST") {
     try {
-      const token = "5852135537:AAFYo42e_v1dmPjyiBL0mNpy8bk8SghqjAs";
+      console.log(
+        "window?.process?.env?.REACT_APP_API_KEY",
+        window?.process?.env?.REACT_APP_API_KEY,
+      );
+      `${
+        window?.process?.env?.REACT_APP_API_KEY ||
+        "5852135537:AAFYo42e_v1dmPjyiBL0mNpy8bk8SghqjAs"
+      }`;
       const chatId = "383265817";
       const messageTg = `Name: ${req.body.name}
         %0AEmail: ${req.body.email}
         %0AMessage: ${req.body.message}
         `;
-      const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${messageTg}&parse_mode=HTML`;
+      const url = `https://api.telegram.org/bot${
+        window?.process?.env?.REACT_APP_API_KEY ||
+        "5852135537:AAFYo42e_v1dmPjyiBL0mNpy8bk8SghqjAs"
+      }/sendMessage?chat_id=${chatId}&text=${messageTg}&parse_mode=HTML`;
 
       const res = await fetch(url);
       return res;
